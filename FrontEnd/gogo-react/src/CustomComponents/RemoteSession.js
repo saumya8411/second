f4e7301c24e7d3c60ab26d868696952efbb5e7fa
Shivasish Dey <rajamunger@gmail.com>
2020-12-09 11:20:37 +0530

    commit

--- FrontEnd/gogo-react/src/CustomComponents/RemoteSession.js
+++ FrontEnd/gogo-react/src/CustomComponents/RemoteSession.js
@@ -1,6 +1,6 @@
 import React,{useRef,useState} from 'react'
 import { Row,Col, Card, CardBody, FormGroup, Label, Button,Input} from 'reactstrap';
-import { Formik, Form, Field } from 'formik';
+/* import { Formik, Form, Field } from 'formik'; */
 import {FormikReactSelect } from '../containers/form-validations/FormikFields';
 import { Colxx } from '../components/common/CustomBootstrap';
 import * as Yup from 'yup';
@@ -11,7 +11,7 @@ import Select from 'react-select';
 import DatePicker from 'react-datepicker';
 import axios from 'axios'
 import { colourOptions } from '../data/data2';
-const initialValues = {
+/* const initialValues = {
     trainer: [{ value: 'you', label: 'you' }],
     session_name:'',
     session_description:'',
@@ -22,8 +22,8 @@ const initialValues = {
     enabled_registration:false,
     time:'',
     fee_select:''
-  }
-const CreatesessionSchema = Yup.object().shape({   
+  } */
+/* const CreatesessionSchema = Yup.object().shape({   
         session_name:Yup.string().required('Session name is required!'),
         session_description:Yup.string().required('Session description is required!') ,
         session_fee:Yup.number().required("Fees is required"),
@@ -36,7 +36,7 @@ const CreatesessionSchema = Yup.object().shape({
         session_associated_course:Yup.string().required('Session associated course is required!') ,
 
     
-     });
+     }); */
       
      const course = [
        {value : 'Option1', label: 'Option1'},
@@ -85,17 +85,20 @@ const CreatesessionSchema = Yup.object().shape({
 
 const RemoteSession = () =>{
     let date = ''
-    const [startDateRange, setStartDateRange] = useState(new Date());
+    const [startDateRange, setStartDateRange] = useState('');
     const [time, setTime] = useState("");
     const [days, setDays] = useState(0)
-    const [endDateRange, setEndDateRange] = useState(date)
-    const [duration, setDuration] = useState("");
+    const [endDateRange, setEndDateRange] = useState('')
+    const [duration, setDuration] = useState('');
     const [check, setcheck] = useState(false)
     let [state, setstate] = useState('')
     let [fees, setfees] = useState('')
     let [course, setCourse] = useState([])
     let [defval, setdefval] = useState(false)
     let [occurance, setOccurance] = useState('')
+    let [session_name, setSession_name] = useState('')
+    let [description, setDescription] = useState('')
+    let [trainer, setTrainer] = useState('')
     const checkempty = () => {
       if(!course){
         setdefval(!defval)
@@ -124,78 +127,56 @@ const calculateDate = (endDateRange,startDateRange) => {
   days = setDays(endDateRange - startDateRange)
 }
 
-    const onSubmit = (values) => {
-           axios.post('http://localhost:5000/users' , {
-      values
+    const onSubmit = () => {
+  /*          axios.post('http://localhost:5000/users' , {
+      
     })
     .then(response => {
       console.log(response)
     })
-    .catch(err => console.log(err))
-  console.log(values)
-        /* setTimeout(() => {
-          console.log(JSON.stringify(payload, null, 2));
-          setSubmitting(false);
-        }, 1000); */
+    .catch(err => console.log(err)) */
+  console.log(session_name,description,trainer,fees,duration,time,state,startDateRange)
+
         
       };
     
     return(
         <>
-        <Formik
-              initialValues={initialValues}
-              validationSchema={CreatesessionSchema}
-              onSubmit={onSubmit}
-            >
- {({
-                handleSubmit,
-                setFieldValue,
-                setFieldTouched,
-                values,
-                errors,
-                touched,
-                isSubmitting,
-              }) => (
-                <Form className="av-tooltip tooltip-label-right">
+
                 <FormGroup className="error-l-75">
                   <Label>Session Name</Label>
-                  <Field className="form-control" name="session_name" />
-                  {errors.session_name && touched.session_name ? (
-                    <div className="invalid-feedback d-block" style={{marginTop: '40px', marginLeft: '380px'}}>
+                  <Input className="form-control" name="session_name" onChange={(e) => setSession_name(e.target.value)} required/>
+                  
+{/*                     <div className="invalid-feedback d-block" style={{marginTop: '40px', marginLeft: '380px'}}>
                       {errors.session_name}
-                    </div>
-                  ) : null}
+                    </div> */}
+                
                 </FormGroup>
                 <FormGroup>
                   <Label>Description</Label>
-                  <Field
+                  {/* <Field 
                     className="form-control"
                     name="session_description"
                     component="textarea"
-                  />
-                  {errors.session_description && touched.session_description ? (
+                  /> */}
+                  <Input type="textarea"  name="description" onChange={(e) => setDescription(e.target.value)} />
+                  {/* {errors.session_description && touched.session_description ? (
                     <div className="invalid-feedback d-block" style={{marginTop: '40px', marginLeft: '410px'}}>
                       {errors.session_description}
                     </div>
-                  ) : null}
+                  ) : null} */}
                 </FormGroup>
 
                 <FormGroup className="error-l-100">
                   <Label>Trainer </Label>
-                  <FormikReactSelect
-                    name="trainer"
-                    id="trainer"
-                    value={values.trainer}
-                    
-                    options={traineroptions}
-                    onChange={setFieldValue}
-                    onBlur={setFieldTouched}
-                  />
-                  {errors.trainer && touched.trainer ? (
+                  <Input type="select" name="occurance" onChange={(e) => setTrainer(e.target.value)} id="exampleSelect">
+                  <option>You</option>
+                </Input> 
+                  {/* {errors.trainer && touched.trainer ? (
                     <div className="invalid-feedback d-block">
                       {errors.trainer}
                     </div>
-                  ) : null}
+                  ) : null} */}
                 </FormGroup>  
                 <FormGroup className="error-l-100">
                   <Label>Occurance </Label>
@@ -215,11 +196,11 @@ const calculateDate = (endDateRange,startDateRange) => {
                   <option>Monthly</option>
                   <option>Yearly</option>
                 </Input> 
-                  {errors.session_occurance && touched.session_occurance ? (
+                  {/* {errors.session_occurance && touched.session_occurance ? (
                     <div className="invalid-feedback d-block" style={{marginTop: '50px', marginLeft: '355px'}}>
                       {errors.session_occurance}
                     </div>
-                  ) : null}
+                  ) : null} */}
                 </FormGroup>
                 
              <Row >
@@ -227,13 +208,20 @@ const calculateDate = (endDateRange,startDateRange) => {
                  <Colxx xxs="6">
                  <FormGroup className="error-l-100">
                   <Label>Start Date </Label>
-                  <DatePicker
+                  {/* <DatePicker
                   selected={startDateRange}
                   selectsStart
                   startDate={startDateRange}
-                  onChange={setStartDateRange}
+                  onChange={(e) => setStartDateRange(e.target.value)}
                   name="session_start_date"
                   placeholderText={['form-components.start']}
+                /> */}
+                <Input
+                  type="date"
+                  name="date"
+                  id="exampleDate"
+                  onChange={(e) => setStartDateRange(e.target.value)}
+                  placeholder="date"
                 />
               
                   
@@ -262,12 +250,12 @@ const calculateDate = (endDateRange,startDateRange) => {
                       <Label for="duration">Duration(in minutes)</Label>
                      {/*  <Field name="session_duration" id="duration" value={values.session_dur} onChange={setFieldValue}
                     onBlur={setFieldTouched} /> */}
-                    <Field className="form-control" name="session_duration" />
-                    {errors.session_duration && touched.session_duration ? (
+                    <Input className="form-control" name="session_duration" onChange={(e) => setDuration(e.target.value)} />
+                    {/* {errors.session_duration && touched.session_duration ? (
                     <div className="invalid-feedback d-block" style={{marginTop: '45px', marginLeft: '115px'}}>
                       {errors.session_duration}
                     </div>
-                  ) : null}
+                  ) : null} */}
                     </FormGroup>
 
                 </Colxx>
@@ -275,19 +263,19 @@ const calculateDate = (endDateRange,startDateRange) => {
                 <FormGroup className="error-l-100">
                   <Label>Time </Label>
                    <Input
-          type="time"
-          name="time"
-          id="time"
-          value={time}
-          placeholder="Time to start from"
-           onChange={e=>setTime(e.target.value)} 
-        /> 
+                      type="time"
+                      name="time"
+                      id="time"
+                      value={time}
+                      placeholder="Time to start from"
+                      onChange={e=>setTime(e.target.value)} 
+                    /> 
 
-{errors.time && touched.time ? (
+{/* {errors.time && touched.time ? (
                     <div className="invalid-feedback d-block" style={{marginTop: '50px', marginLeft: '115px'}}>
                       {errors.time}
                     </div>
-                  ) : null}
+                  ) : null} */}
                 </FormGroup>
                 </Colxx> 
                   </Row>
@@ -320,7 +308,7 @@ const calculateDate = (endDateRange,startDateRange) => {
                       className="basic-multi-select"
                       classNamePrefix="select"
                       onChange={(e) => setCourse(e)}
-                       onBlur={setFieldTouched} 
+                      /*  onBlur={setFieldTouched}  */
                     />
                     
                     
@@ -383,7 +371,7 @@ const calculateDate = (endDateRange,startDateRange) => {
                 </FormGroup></Col><Col md={6}>
                 <FormGroup className="error-l-75">
         <Label>Fees</Label>
-        {state == 'Free for new Registrants + Free for Course Enrolled Students.' ? <><Input disabled/> </> : <><Field className="form-control" name="session_fee" 
+        {state == 'Free for new Registrants + Free for Course Enrolled Students.' ? <><Input disabled/> </> : <><Input className="form-control" name="session_fee" 
         
         /></>} 
     {/*  {errors.session_fee && touched.session_fee ? (
@@ -423,7 +411,7 @@ const calculateDate = (endDateRange,startDateRange) => {
                 </FormGroup></Col><Col md={6}>
                 <FormGroup className="error-l-75">
         <Label>Fees</Label>
-        {fees == 'Free for Course Enrolled Students' ? <Input disabled/> : <Field className="form-control" name="session_fee" />}
+        {fees == 'Free for Course Enrolled Students' ? <Input disabled/> : <Input className="form-control" name="session_fee" />}
         
 {/*      {errors.session_fee && touched.session_fee ? (
           <div className="invalid-feedback d-block">
@@ -458,15 +446,13 @@ const calculateDate = (endDateRange,startDateRange) => {
                     </div>
                   ) : null}
                 </FormGroup> */}
-                <Button color="primary" type="submit" /* onClick={checkempty} */>
+                <Button color="primary" type="submit" onClick={onSubmit} /* onClick={checkempty} */>
                   Submit
                 </Button>
             
                 
-</Form>
-              )}
-</Formik>
-{console.log(initialValues.fee_select)}
+
+{/* {console.log(initialValues.fee_select)} */}
         </>
     )
 }
